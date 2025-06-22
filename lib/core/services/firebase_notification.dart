6ad10fe42933bfc98229c211b8258cfa0f35ef1e
mohamedabdelnaser <mohamedabdelnaser782@gmail.com>
2025-06-23 01:05:22 +0300

    build

--- lib/core/services/firebase_notification.dart
+++ lib/core/services/firebase_notification.dart
@@ -2,6 +2,7 @@
 import 'dart:convert';
 import 'dart:io';
 
+import 'package:firebase_config/firebase_config.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:firebase_messaging/firebase_messaging.dart';
 import 'package:flutter/cupertino.dart';
@@ -133,7 +134,7 @@ class FirebaseNotifications {
       RemoteMessage message) async {
     // If you're going to use other Firebase services in the background, such as Firestore,
     // make sure you call `initializeApp` before using other Firebase services.
-    await Firebase.initializeApp();
+    FirebaseBootstrapper.initFirebase(languageCode: '');
     print('Handling a background message ${message.messageId}');
   }
 
