5cfb118d904248820a33518ea9b295b70ee095fb
mohamedfarag <mfarag.c@nwc.com.sa>
2026-06-10 01:43:42 +0300

    build ios

--- macos/Flutter/ephemeral/flutter_export_environment.sh
+++ macos/Flutter/ephemeral/flutter_export_environment.sh
@@ -1,11 +1,11 @@
 #!/bin/sh
 # This is a generated file; do not edit or check into version control.
-export "FLUTTER_ROOT=H:\flutter_versions\flutter_windows_3.35.5-stable\flutter"
-export "FLUTTER_APPLICATION_PATH=H:\Projects\Flutter\support\cashift"
+export "FLUTTER_ROOT=/Users/mohamedfarag/fvm/versions/3.35.7"
+export "FLUTTER_APPLICATION_PATH=/Users/mohamedfarag/StudioProjects/cashift"
 export "COCOAPODS_PARALLEL_CODE_SIGN=true"
 export "FLUTTER_BUILD_DIR=build"
-export "FLUTTER_BUILD_NAME=1.5.0"
-export "FLUTTER_BUILD_NUMBER=117"
+export "FLUTTER_BUILD_NAME=1.5.1"
+export "FLUTTER_BUILD_NUMBER=118"
 export "DART_OBFUSCATION=false"
 export "TRACK_WIDGET_CREATION=true"
 export "TREE_SHAKE_ICONS=false"
